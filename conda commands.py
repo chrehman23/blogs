29e6a459a041335255c53b65991af2957d242163
Abdur Rehman <51826277+chrehman23@users.noreply.github.com>
2025-07-10 15:52:45 +0400

    Create conda commands.py

---
+++ conda commands.py
@@ -0,0 +1,4 @@
+conda list # list all isntalled packeges 
+conda env list # list all enverments
+conda create -n NAME_OF_ENVERMENT # To create enverment 
+conda activate NAME_OF_ENVERMENT # to activeate test 
